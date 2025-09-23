c2153e1ba0e55d6810cb6dd237921ef8e8249a22
Malahov42 <inteligentishe@yandex.ru>
2025-09-23 08:14:55 +0400

    Add files via upload

---
+++ lab02_ind_v3/task1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(0, "");
+	int m;
+	cout << "Введите номер месяца:";
+	cin >> m;
+	switch (m) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			cout << "В месяце 31 день"<<endl; break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			cout << "В месяце 30 день" << endl; break;
+		case 2:
+			cout << "В месяце 28 дней" << endl; break;
+		default:
+			cout << "Такого месяца нет :(";
+	}
+	return 0;
+
+}
---
+++ lab02_ind_v3/task4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int s;
+    cout << "Введите количество кораблей: ";
+    cin >> s;
+    string ending;
+    if (s % 10 == 1 && s % 100 != 11) {
+        ending = "ь";
+    }
+    else if ((s % 10 >= 2 && s % 10 <= 4) && (s % 100 < 12 || s % 100 > 14)) {
+        ending = "я";
+    }
+    else {
+        ending = "ей";
+    }
+    cout << "Джек утопил " << s << " корабл" << ending << endl;
+    return 0;
+}
